d2fe9c5567b61ff768a53f70783dcfc0eada2e32
MillenniumRain <nik210597@gmail.com>
2024-12-28 13:30:07 +0500

    deploy test_1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
 	},
+	plugins: [reactRefresh()],
 });
